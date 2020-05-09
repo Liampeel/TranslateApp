7f67b7f60391698cabe92e013b13b23626e282dc
kieron <sc16kh@leeds.ac.uk>
2020-05-09 15:01:52 +0100

    fixed bug in urls file

--- djangoAPI/queries/urls.py
+++ djangoAPI/queries/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from queries import views
 from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
-    path('queries/', views.query_list),
-    path('queries/<int:pk>/', views.query_detail),
+    path('queries/', views.QueryList.as_view()),
+    path('queries/<int:pk>/', views.QueryDetail.as_view()),
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
 ]
