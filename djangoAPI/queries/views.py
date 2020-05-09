feda06641a496b0e720d92ba52581db59a230f16
kieron <sc16kh@leeds.ac.uk>
2020-05-09 12:31:01 +0100

    Updated views to use named status codes

--- djangoAPI/queries/views.py
+++ djangoAPI/queries/views.py
@@ -15,15 +15,15 @@ def query_list(request):
     if request.method == 'GET':
         queries = Query.objects.all()
         serializer = QuerySerializer(queries, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = QuerySerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def query_detail(request, pk):
@@ -33,20 +33,20 @@ def query_detail(request, pk):
     try:
         query = Query.objects.get(pk=pk)
     except Query.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = QuerySerializer(query)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
         serializer = QuerySerializer(query, data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         query.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
