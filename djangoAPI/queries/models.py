f68d6da51cc0c1668771d68b62e522e34f5abdd9
kieron <sc16kh@leeds.ac.uk>
2020-05-09 12:42:55 +0100

    inital setup for users

--- djangoAPI/queries/models.py
+++ djangoAPI/queries/models.py
@@ -5,7 +5,11 @@ from pygments.styles import get_all_styles
 
 class Query(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('auth.User', related_name='queries', on_delete=models.CASCADE)
     user_query = models.TextField()
+
+    def save(self, *args, **kwargs):
+        super(Query, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ['date_created']
