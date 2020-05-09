c294c58de57f6d528fc7c57bb49016ee6286fa80
kieron <sc16kh@leeds.ac.uk>
2020-05-09 11:54:18 +0100

    Query model created

--- djangoAPI/queries/models.py
+++ djangoAPI/queries/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 # Create your models here.
+
+class Query(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    user_query = models.TextField()
+
+    class Meta:
+        ordering = ['date_created']
