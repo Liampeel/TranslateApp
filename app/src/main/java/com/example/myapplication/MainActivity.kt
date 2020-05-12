b296c7b0819889408eda205dfe46e67cbb6667de
connor <cradams@hotmail.co.uk>
2020-05-12 17:34:05 +0100

    Fixed it so that user must log in to access OCR

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -25,38 +25,37 @@ import okhttp3.ResponseBody
 
 
 lateinit var username: EditText
-    lateinit var password: EditText
+lateinit var password: EditText
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onDestroy(){
+    override fun onDestroy() {
         super.onDestroy()
-        System.out.println("destroy")
-        var token = ("Token "+ SharedPrefManager.getInstance(applicationContext).fetchAuthToken())
+        println("destroy")
+        val token = ("Token " + SharedPrefManager.getInstance(applicationContext).fetchAuthToken())
 
+        println(token)
 
+        RetrofitClient.getInstanceToken(token)?.api?.logout()
 
-        System.out.println(token)
-
-        com.example.myapplication.API.RetrofitClient.getInstanceToken(token)?.api?.logout()
-
-            ?.enqueue(object: Callback<ResponseBody> {
+            ?.enqueue(object : Callback<ResponseBody> {
                 override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
                     Toast.makeText(applicationContext, "Error", Toast.LENGTH_SHORT).show()
                     println("No response from server")
                 }
 
-                override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>
+                override fun onResponse(
+                    call: Call<ResponseBody>, response: Response<ResponseBody>
                 ) {
                     println("got response ")
                     if (response.code() == 200) {
-                        println("respomnse code is 201")
+                        println("Response code is: ${response.code()}")
                         if (response.body() != null) {
-                            println("sending translation")
+                            println("Sending translation")
                             SharedPrefManager.getInstance(this@MainActivity).clear()
                             val intent = Intent(this@MainActivity, MainActivity::class.java)
                             startActivity(intent)
-                            Toast.makeText(applicationContext, "success", Toast.LENGTH_SHORT)
+                            Toast.makeText(applicationContext, "Success", Toast.LENGTH_SHORT)
                                 .show()
                         }
                     } else {
@@ -98,8 +97,10 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun userLogin() {
-        val intent = Intent(this, OCRActivity::class.java)
-        startActivity(intent)
+//        val intent = Intent(this, OCRActivity::class.java)
+//        startActivity(intent)
+
+        var loggedIn = false
 
         val username = usernameText.text.toString()
         val password = editTextPassword.text.toString().trim()
@@ -115,41 +116,60 @@ class MainActivity : AppCompatActivity() {
             editTextPassword.requestFocus()
 
         }
+        if (password.length < 5) {
+            editTextPassword.error = "Password length is insufficient"
+            editTextPassword.requestFocus()
 
+        } else {
 
-
-
-
-        
-
-        loginClient.RetrofitClient.instance.loginUser(loginData(username, password))
-            .enqueue(object : Callback<loginResponse> {
-                override fun onFailure(call: Call<loginResponse>, t: Throwable) {
-                    Toast.makeText(applicationContext, "Failure", Toast.LENGTH_SHORT).show()
-                }
-
-                override fun onResponse(
-                    call: Call<loginResponse>,
-                    response: Response<loginResponse>
-                ) {
-                    if (response.code() == 200) {
-                        if (response.body() != null) {
-                            val loginResponse = response.body()
-                            Toast.makeText(applicationContext, loginResponse!!.token, Toast.LENGTH_SHORT)
-                                .show()
-                            SharedPrefManager.getInstance(applicationContext).saveAuthToken(loginResponse!!.token)
-
-                        }
-                    } else {
-                        Toast.makeText(applicationContext, "error logging in", Toast.LENGTH_SHORT
-                        ).show()
+            val intent = Intent(this, OCRActivity::class.java)
+            loginClient.RetrofitClient.instance.loginUser(loginData(username, password))
+                .enqueue(object : Callback<loginResponse> {
+                    override fun onFailure(call: Call<loginResponse>, t: Throwable) {
+                        Toast.makeText(applicationContext, "Failure", Toast.LENGTH_SHORT).show()
                     }
-                }
 
-            })
+                    override fun onResponse(
+                        call: Call<loginResponse>,
+                        response: Response<loginResponse>
+                    ) {
+                        if (response.code() == 200) {
+                            if (response.body() != null) {
+                                val loginResponse = response.body()
+
+                                loggedIn = true
+
+//                            loginResponse!!.token
+                                Toast.makeText(
+                                    applicationContext,
+                                    R.string.loginSuccess,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                if (loggedIn) {
+                                    startActivity(intent)
+                                }
+
+
+                                if (loginResponse != null) {
+                                    SharedPrefManager.getInstance(applicationContext).saveAuthToken(
+                                        loginResponse.token
+                                    )
+                                }
+                            }
+
+                        } else {
+                            Toast.makeText(
+                                applicationContext, "Error logging in", Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+
+                })
+        }
     }
 
-    }
+}
 
 
 
